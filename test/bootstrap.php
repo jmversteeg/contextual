b2451d3975ffa3c6113a759a48c26141c41aebd0
jmversteeg <janmerijnversteeg@gmail.com>
2015-04-03 14:25:42 +0200

    Rename lib/ to src/

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,5 +4,5 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $loader = new \Aura\Autoload\Loader();
 $loader->addPrefix('test\jmversteeg\contextual', __DIR__ . '/jmversteeg/contextual');
-$loader->addPrefix('jmversteeg\contextual', __DIR__ . '/../lib/jmversteeg/contextual');
+$loader->addPrefix('jmversteeg\contextual', __DIR__ . '/../src/jmversteeg/contextual');
 $loader->register();
