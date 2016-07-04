0d18cd35518dc4b9e362f9d4210d9415138ef1a8
JM Versteeg <janmerijnversteeg@gmail.com>
2016-07-04 17:53:29 +0200

    Formatting

--- test/jmversteeg/contextual/helper/TypedContext.php
+++ test/jmversteeg/contextual/helper/TypedContext.php
@@ -4,10 +4,14 @@ namespace test\jmversteeg\contextual\helper;
 
 use jmversteeg\contextual\Context;
 
+/**
+ * @property string  $type     Type
+ * @property boolean $beepboop Something beepy is coming this way
+ */
 class TypedContext extends Context
 {
 
-    private $_type = 'foo';
+    private $_type     = 'foo';
     private $_beepboop = false;
 
 }
